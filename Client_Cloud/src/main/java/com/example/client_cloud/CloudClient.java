325249d9f3a97a1b6b7a5c9baeda769da1b9e183
EnoticSho <paradise666hell@mail.ru>
2022-10-07 10:27:57 +0300

    added css styles

--- Client_Cloud/src/main/java/com/example/client_cloud/CloudClient.java
+++ Client_Cloud/src/main/java/com/example/client_cloud/CloudClient.java
@@ -11,7 +11,7 @@ public class CloudClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CloudClient.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 182, 340);
+        Scene scene = new Scene(fxmlLoader.load(), 505, 325);
         stage.setTitle("Cloud Client");
         stage.setScene(scene);
         stage.show();
